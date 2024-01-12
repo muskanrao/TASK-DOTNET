90aa4e67545160d2011a5b1f6b462ebac8c29eaf
Muskan Rao <88151137+muskanrao@users.noreply.github.com>
2024-01-12 11:11:48 +0530

    Update IBankRepository.cs

---
+++ BankProject/IBankRepository.cs
@@ -0,0 +1,12 @@
+namespace BankProject
+{
+    public interface IBankRepository
+    {
+        void NewAccount(SBAccount acc);
+        List<SBAccount> GetAllAccounts();
+        SBAccount GetAccountDetails(int accno);
+        void DepositAmount(int accno, decimal amt);
+        void WithdrawAmount(int accno, decimal amt);
+        List<SBTransaction> GetTransactions(int accno);
+}
+}
